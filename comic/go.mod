d14889dcafe8efbeacbeb0e3a362e2acafe703d8
chengche123 <1107640282@qq.com>
2021-05-16 15:58:07 +0800

    model直接内嵌crawler里的

--- comic/go.mod
+++ comic/go.mod
@@ -6,8 +6,11 @@ go 1.16
 
 replace comic => ../
 
+replace crawler => ../../crawler
+
 require (
 	comic v0.0.0-00010101000000-000000000000
+	crawler v0.0.0-00010101000000-000000000000
 	github.com/golang/protobuf v1.5.2
 	github.com/google/gofuzz v1.0.0
 	github.com/micro/go-micro/v2 v2.9.1
--- comic/model/comic.go
+++ comic/model/comic.go
@@ -1,36 +1,17 @@
 package model
 
+import (
+	cmodel "crawler/model/comic"
+)
+
 type ComicDetail struct {
-	ID                    int    `json:"Id" gorm:"primaryKey;autoIncrement;not null"`
-	Title                 string `json:"Title"`
-	Direction             int    `json:"Direction"`
-	Islong                int    `json:"Islong"`
-	Cover                 string `json:"Cover"`
-	Description           string `json:"Description"`
-	Lastupdatetime        int    `json:"LastUpdatetime"`
-	Lastupdatechaptername string `json:"LastUpdateChapterName"`
-	Firstletter           string `json:"FirstLetter"`
-	Comicpy               string `json:"ComicPy"`
-	Hotnum                int    `json:"HotNum"`
-	Hitnum                int    `json:"HitNum"`
-	Lastupdatechapterid   int    `json:"LastUpdateChapterId"`
-	Subscribenum          int    `json:"SubscribeNum"`
+	cmodel.ComicDetail
 }
 
 type CategoryDetail struct {
-	ID             int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Title          string `json:"title"`
-	Authors        string `json:"authors"`
-	Status         string `json:"status"`
-	Cover          string `json:"cover"`
-	Types          string `json:"types"`
-	LastUpdatetime int    `json:"last_updatetime"`
-	Num            int    `json:"num"`
+	cmodel.CategoryDetail
 }
 
 type ComicCategoryFilter struct {
-	Id      int64  `gorm:"primaryKey;autoIncrement;not null"`
-	Title   string `gorm:"not null;type:varchar(255)"`
-	TagID   int
-	TagName string
+	cmodel.ComicCategoryFilter
 }
