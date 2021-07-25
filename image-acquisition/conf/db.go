75a479d3b171298a5cee79f9569d678ae13972e3
chengche123 <1107640282@qq.com>
2021-07-25 12:14:58 +0800

    重新组织目录

--- image-acquisition/conf/db.go
+++
@@ -1,3 +0,0 @@
-package conf
-
-const DSN = "root:qwe123456@tcp(47.96.152.251:3306)/image_acquisition"
--- image-acquisition/model/user.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type User struct {
-	Id       int64  `gorm:"primaryKey;autoIncrement;not null"`
-	UserName string `gorm:"uniqueIndex;not null;type:varchar(255)"`
-	Password string `gorm:"not null;type:varchar(255)"`
-}
